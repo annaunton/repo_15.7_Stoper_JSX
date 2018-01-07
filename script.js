d3e8f436bcb127e6457a6758db156a87ab284139
ani <anna.unton@yahoo.com>
2018-01-07 17:31:43 +0700

    initial commit

--- script.js
+++ script.js
@@ -1,74 +1,60 @@
-//var element = React.createElement('div', {}, 'Hello world!');
+var Movie = React.createClass({
+  
+  propTypes: {
+    movie: React.PropTypes.object.isRequired,
+  },
 
-
-/*var element =
-	React.createElement('div', {}, 
-		React.createElement('h1', {}, 'Lista filmów'),
-		React.createElement('ul', {},
-			React.createElement('li', {},
-				React.createElement('h1', {}, 'Harry Poter'),
-				React.createElement('p', {}, 'Film o czarodzieju'),
-			),
-			React.createElement('li', {}, 
-				React.createElement('h1', {}, 'Król Lew'),
-				React.createElement('p', {}, 'Film o królu sawanny'),
-				React.createElement('img', {src: 'cat.jpg'},)
-
-			),
-
-		)
-	);
-
-
-ReactDOM.render(element, document.getElementById('app'));
-*/
-
-
-
-var movies = [
-{
-	id: 1,
-	title : 'Harry Potter',
-	desc : 'film o czarodzieju',
-	img : 'src/harry.jpg'
-},
-{
-	id: 2,
-	title : 'Król Lew',
-	desc : 'film o królu sawanny',
-	img : 'src/cat.jpg'
-},
-{
-	id : 3,
-	title : 'Pożegnanie z Afryką',
-	desc : 'film o Afryce',
-	img : 'src/africa.jpg'
-},
-{
-	id : 4,
-	title : 'Dunkierka',
-	desc : 'film o ewakuacji Dunkierki',
-	img : 'src/dunkierka.jpg'
-},
-];
-
-var moviesElements = movies.map(function(movie) {
-	return React.createElement('li', {key: movie.id},
-		React.createElement('h2', {}, movie.title),
-		React.createElement('p', {}, movie.desc),
-		React.createElement('img', {src:movie.img, width:'400px'},)
-
-	);
-
+  render: function() {
+    return (
+      React.createElement('li', {key: this.props.movie.id},
+        React.createElement('h2', {}, this.props.movie.title),
+        React.createElement('p', {}, this.props.movie.desc),
+        React.createElement('img', {src:this.props.movie.img})
+      )
+    )
+  },
 });
 
-var element = 
-	React.createElement('div', {},
-		React.createElement('h1', {}, 'Lista filmów'),
-		React.createElement('ul', {}, moviesElements),
+var movie1 = {
+  id: 1,
+  title : 'Harry Potter',
+  desc : 'film o czarodzieju',
+  img : 'src/harry.jpg'
+};
+var movie2 = {
+  id: 2,
+  title : 'Harry Potter',
+  desc : 'film o czarodzieju',
+  img : 'src/harry.jpg'
+};
 
 
-);
-
+var element = React.createElement(Movie, {movie: movie1, movie: movie2});
 ReactDOM.render(element, document.getElementById('app'));
 
+/*
+var GalleryItem = React.createClass({
+  
+  propTypes: {
+    image: React.PropTypes.object.isRequired,
+  },
+
+  render: function() {
+    return (
+      React.createElement('div', {},
+        React.createElement('h2', {}, this.props.image.name),
+        React.createElement('img', {src: this.props.image.src})
+      )
+    )
+  },
+});
+
+var image1 = {
+  name: 'Kotek',
+  src: 'http://imgur.com/n8OYCzR.png'
+};
+
+
+var element = React.createElement(GalleryItem, {image: image1});
+ReactDOM.render(element, document.getElementById('app'));
+*/
